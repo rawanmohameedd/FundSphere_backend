31f6558adb57d5bfad483486507a15700848c962
rawanmohameedd <nrawana12345@gmail.com>
2024-10-09 12:00:05 +0300

    add final postman collection

--- Src/Models/donations.js
+++ Src/Models/donations.js
@@ -78,6 +78,7 @@ async function getDonations_byCampaigns (campaign_id){
                 amount: true,
                 donation_date: true,
                 user: true,
+                campaign: true
             }
         })
 
