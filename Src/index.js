58960d7b2cab85bea2c89c0864e80b358939977e
rawanmohameedd <nrawana12345@gmail.com>
2024-10-09 13:21:10 +0300

    final

--- Src/index.js
+++ Src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require("path")
 const fileUpload = require('express-fileupload');
 const userRouter = require('./Routes/users');
 const campaignRouter = require('./Routes/campaigns')
@@ -14,6 +15,11 @@ app.use(express.json());
 app.use(userRouter);
 app.use(campaignRouter)
 app.use(donationRouter)
+
+app.use(express.static(path.join(__dirname, '../../FundSphere_frontend/FundSphere/dist')))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, '../../FundSphere_frontend/FundSphere', 'dist','index.html'))
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
